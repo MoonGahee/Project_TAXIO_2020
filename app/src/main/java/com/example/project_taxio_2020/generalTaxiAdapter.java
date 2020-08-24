11880f3ceee21bd009e671079302056199921d8f
KooHaeun <heni1325@gmail.com>
2020-08-24 17:42:16 +0900

    taxi recycler하는 중

---
+++ app/src/main/java/com/example/project_taxio_2020/generalTaxiAdapter.java
@@ -0,0 +1,6 @@
+package com.example.project_taxio_2020;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class generalTaxiAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/example/project_taxio_2020/generalTaxiItem.java
@@ -0,0 +1,13 @@
+package com.example.project_taxio_2020;
+
+public class generalTaxiItem {
+    private String tripDate;
+
+    public String getTripDate() { //기사 이름
+        return tripDate;
+    }
+
+    public void setTripDate(String tripDate) {
+        this.tripDate = tripDate;
+    }
+}
