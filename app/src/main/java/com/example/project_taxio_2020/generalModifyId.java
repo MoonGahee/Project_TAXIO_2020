3bf06680ffed7fd81b3774952e76042c6de3117c
MoonGahee <moongahee12@gmail.com>
2020-10-26 07:10:34 +0900

    데이터 데려오는 과정 코딩 중

--- app/src/main/java/com/example/project_taxio_2020/generalModifyId.java
+++ app/src/main/java/com/example/project_taxio_2020/generalModifyId.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -82,6 +83,21 @@ public class generalModifyId extends AppCompatActivity {
 
         photo = findViewById(R.id.photoMod);
 
+        ValueEventListener generalListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                General general = snapshot.getValue(General.class);
+                //읽어오는 거 해야징
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //없는 경우
+            }
+        };
+        mDatabase.addValueEventListener(generalListener);
+
+
         btnComplete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,44 +122,7 @@ public class generalModifyId extends AppCompatActivity {
                 result.put("general_email", getGeneral_email);
 
                 mDatabase.child(getGeneral_id).setValue(result); //해당 데이터의 자식
-                /*mDatabase.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                       General general = snapshot.getValue(General.class);
 
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });*/
-                /*mDatabase.addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });*/
 
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
