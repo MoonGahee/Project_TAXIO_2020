8bd684d1c95d4ec62f25cb1d172e776685fb659d
KooHaeun <heni1325@gmail.com>
2020-11-09 18:33:15 +0900

    예외처리 추가 -> 대여시간 별 시작시간 설정 범위 네비하는 중

--- app/src/main/java/com/example/project_taxio_2020/generalCheckEpilogueActivity.java
+++ app/src/main/java/com/example/project_taxio_2020/generalCheckEpilogueActivity.java
@@ -5,18 +5,28 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
 // 관우
 
 public class generalCheckEpilogueActivity extends AppCompatActivity {
     Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    NavigationView nDrawer;
     TextView title_text;
     Button edit_epilogue;
     ListView listView;
@@ -28,6 +38,11 @@ public class generalCheckEpilogueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.general_check_epilogue_activity);
         setToolbar();
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        nDrawer = (NavigationView)findViewById(R.id.nDrawer);
+        naviItem();
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
 
         edit_epilogue = findViewById(R.id.edit_epilogue);
         listView = findViewById(R.id.epilogues);
@@ -59,6 +74,36 @@ public class generalCheckEpilogueActivity extends AppCompatActivity {
         });
     }
 
+    public void naviItem(){
+        nDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() { //Navigation Drawer 사용
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+
+                if(id == R.id.drawer_schTrip){
+                    Intent intent = new Intent(getApplicationContext(), generalSDriverActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else if (id == R.id.drawer_myInfo) {
+                    Intent intent = new Intent(getApplicationContext(), generalCheckEpilogueActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else if (id == R.id.drawer_modify) {
+                    Intent intent = new Intent(getApplicationContext(), generalModifyId.class);
+                    startActivity(intent);
+                    finish();
+                } else if (id == R.id.drawer_out) {
+                    Intent intent = new Intent(getApplicationContext(), generalWriteWithdrawalActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                return true;
+            }
+        });
+    }
     public boolean onOptionsItemSelected(MenuItem item) {//toolbar의 back키 눌렀을 시
         switch (item.getItemId()){
             case android.R.id.home:{//이전 화면으로 돌아감
@@ -71,6 +116,13 @@ public class generalCheckEpilogueActivity extends AppCompatActivity {
 
     public void setToolbar(){
         Toolbar toolbar = (Toolbar)findViewById(R.id.bar); // 툴바를 액티비티의 앱바로 지정 왜 에러?
+        ImageButton menu = findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
         setSupportActionBar(toolbar); //툴바를 현재 액션바로 설정
         ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
         actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제 ~~~~~~~ 왜 에러냐는거냥!!
