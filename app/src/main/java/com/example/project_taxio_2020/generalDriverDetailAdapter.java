e0d585e0f2d0fef2116fd8bd89fb0941fba17b59
MoonGahee <moongahee12@gmail.com>
2020-11-15 07:20:41 +0900

    타임라인 수정 및 기사요청 진행

---
+++ app/src/main/java/com/example/project_taxio_2020/generalDriverDetailAdapter.java
@@ -0,0 +1,10 @@
+package com.example.project_taxio_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate;
+
+// 기사 선택 RecyclerView Detail by 가희
+
+//public class generalDriverDetailAdapter extends RecyclerView.Adapter<generalDriverDetailAdapter.ItemViewHolder> {
+//}
