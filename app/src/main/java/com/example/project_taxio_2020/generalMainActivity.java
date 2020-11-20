6dea331bbf9fa043edca9a18fd78bd4808a7577b
MoonGahee <moongahee12@gmail.com>
2020-11-21 02:23:14 +0900

    날짜 오밭탱구리

--- app/src/main/java/com/example/project_taxio_2020/generalMainActivity.java
+++ app/src/main/java/com/example/project_taxio_2020/generalMainActivity.java
@@ -230,6 +230,7 @@ public class generalMainActivity extends AppCompatActivity {
 
                 if(id == R.id.drawer_schTrip){
                     Intent intent = new Intent(getApplicationContext(), generalMyscheActivity.class);
+                    intent.putExtra("general_num", general_num);
                     startActivity(intent);
                     finish();
                 } else if (id == R.id.drawer_myInfo) {
