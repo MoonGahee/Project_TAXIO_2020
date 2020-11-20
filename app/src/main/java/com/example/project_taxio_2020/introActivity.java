fd3fdeadeb558f4f2c2dd0e1779649e232ef4724
Choi-jinuk <wlsdnr7536@naver.com>
2020-11-21 02:15:05 +0900

    자동로그인 수정_최종2

--- app/src/main/java/com/example/project_taxio_2020/introActivity.java
+++ app/src/main/java/com/example/project_taxio_2020/introActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
